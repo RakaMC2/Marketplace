b3e4cd3070453aa5fcb54022ccaefbce3870f859
RakzVolt <cvrakastoreid@gmail.com>
2026-01-28 18:47:42 +0700

    Add database URL to firebase configuration
    Updated firebase configuration with database URL.

--- services/firebase.ts
+++ services/firebase.ts
@@ -5,13 +5,13 @@ declare var firebase: any;
 const firebaseConfig = {
   apiKey: "AIzaSyAr37SrpRBWv4LPiExlJG0aJT0r5EJ9wCg",
   authDomain: "voltra-essentials.firebaseapp.com",
+  databaseURL: "https://voltra-essentials-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "voltra-essentials",
   storageBucket: "voltra-essentials.firebasestorage.app",
   messagingSenderId: "956503858766",
   appId: "1:956503858766:web:46bf6f30ff0c709fe6ed1f",
   measurementId: "G-K6FEG8DPV2"
 };
-
 // Use checking to prevent double initialization in some environments
 const app = !firebase.apps.length 
   ? firebase.initializeApp(firebaseConfig) 
